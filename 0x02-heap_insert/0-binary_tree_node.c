65336791a458b21401f34014f6b48377d3f13ffb
ameurbesbes <ameurbesbes@outlook.com>
2022-02-22 15:44:18 +0100

    Heap Insert

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,28 +1,20 @@
 #include "binary_trees.h"
-#include<stdio.h>
 /**
- * binary_tree_node - Adds a new node to a binary tree
- * @parent: parent of the created node
- * @value: int: the data of the node
- * Return: pointer to the created node
+ * binary_tree_node - creates a binary tree node
+ * @parent:pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: binary tree node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	
-	binary_tree_t *node;
-	
-	/* Save space in memory */
-	node = malloc (sizeof(binary_tree_t));
-	
-	/* Error case */
-	if(!node)
-		return (0);
-	
-	/* Create new node */
-	node->parent = parent;
-	node->n = value;
-	
-	/* Return */
-	return (node);
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new  == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->left = NULL;
+	return (new);
 }
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,15 +1,6 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
@@ -27,15 +18,9 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
-
-
-/********* Prototypes *******/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-
-/********* Header files  of the standard library *************/
-#include <stdlib.h>
-
-#endif
+heap_t *heap_insert(heap_t **root, int value);
+#endif /* _BINARY_TREES_H_ */
