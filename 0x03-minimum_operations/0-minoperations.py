02fc093bebfd8b7a46cd0b93481bcff6056a8857
ameurbesbes <ameurbesbes@outlook.com>
2022-03-01 11:45:18 +0100

    minimum operation

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""
-Minimum Operations
-"""
 
-
-def isPremium(n):
-    """ Checks if a number is premium """
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
+""" Calculates the fewest number of operations needed to result in exactly n H characters. """
 def minOperations(n):
-    """ defines minimum operations"""
-    if n < 2:
+    """ Calculates the fewest number of operations needed to result in exactly n H characters. """
+    copy = "H"
+    output = ""
+    i = 0
+    if (n < 2):
         return 0
-    i = 2
-    opp = 0
-    while(n != 1):
-        if (isPremium(i)):
-            if (n % i == 0):
-                n = n / i
-                opp = opp + i
-            else:
-                i += 1
+    while (len(output) < n):
+        if (i % 2 == 0):
+            output += copy
         else:
-            i += 1
-    return
+            copy = output
+            output += copy
+        i += 1
+    if (i == n):
+        return i
+    return i + 1
