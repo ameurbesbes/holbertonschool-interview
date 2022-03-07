9b550a2fe039fb1813b2d6d7afe47f629581dd1b
ameurbesbes <ameurbesbes@outlook.com>
2022-03-07 10:39:43 +0100

    first commit

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,18 +1,26 @@
 #include "lists.h"
 /**
- * check_cycle - checks if a singly linked list has a cycle in it.
- * @list: linked list to check.
- * Return: returns 1 if there is a cycle otherwise 0
-**/
-int check_cycle(listint_t *list){
-	listint_t *head = list;
+ * check_cycle - check if linked list has cycle
+ * @list: single linked list
+ * Return:0 or  1
+ */
+int check_cycle(listint_t *list)
+{
 
-	if (list == NULL)
+	listint_t *temp, *check;
+
+	if (!list)
 		return (0);
-	while (list->next){
-		if (list->next == head)
+
+	temp = list;
+	check = list->next;
+	while (temp && check && check->next)
+	{
+		temp = temp->next;
+		check = check->next->next;
+
+		if (check == temp)
 			return (1);
-		list = list->next;
 	}
 	return (0);
 }
